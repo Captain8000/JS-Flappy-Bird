1174d6030c73f099c185427fb10923086097d844
Captain <122565289+Captain8000@users.noreply.github.com>
2025-02-23 00:47:40 +0530

    game.js

--- game.js
+++ game.js
@@ -120,7 +120,7 @@ const pipe = {
 };
 const bird = {
   // Added scale property for resizing the bird image
-  scale: 1,
+  scale: 0.4,
   animations: [
     { sprite: new Image() },
     { sprite: new Image() },
